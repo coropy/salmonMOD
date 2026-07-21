8088653d9dbf340b7dccde1e976dc328c7311982
coropy <alpha.coropy@gmail.com>
2026-07-21 09:13:53 +0900

    template

---
+++ salmon-template-26.2/src/client/java/yam/salmon/client/SalmonClient.java
@@ -0,0 +1,10 @@
+package yam.salmon.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SalmonClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
